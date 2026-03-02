61e2546afab61b61bb9abbe69419aef6e1c9df5c
sofa <sophiapark0@proton.me>
2026-03-01 20:15:07 -0800

    fix indent

--- original-assets/fonts/classicube/resize-accented.py
+++ original-assets/fonts/classicube/resize-accented.py
@@ -11,14 +11,14 @@ left_pad = 0
 right_pad = 1
 
 for filename in os.listdir(accented_font_dir):
-    if filename.endswith(".png"):
-        img_path = os.path.join(accented_font_dir, filename)
-        img = Image.open(img_path)
+	if filename.endswith(".png"):
+		img_path = os.path.join(accented_font_dir, filename)
+		img = Image.open(img_path)
 
-        new_width = img.width + left_pad + right_pad
-        new_height = img.height + top_pad + bottom_pad
+		new_width = img.width + left_pad + right_pad
+		new_height = img.height + top_pad + bottom_pad
 
-        new_img = Image.new("RGBA", (new_width, new_height), (0, 0, 0, 0))
-        new_img.paste(img, (left_pad, top_pad))
+		new_img = Image.new("RGBA", (new_width, new_height), (0, 0, 0, 0))
+		new_img.paste(img, (left_pad, top_pad))
 
-        new_img.save(img_path)
+		new_img.save(img_path)
